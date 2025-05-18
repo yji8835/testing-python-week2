376e1c68f19ac283f53c667cea4c2711b58d7c2c
willook <jonghwa@actnova.io>
2025-05-18 15:10:56 +0900

    docs: face_db 주석

--- app/face/face_db.py
+++ app/face/face_db.py
@@ -1,13 +1,16 @@
-DB_PATH = "face_db.db"
+DB_PATH = "face_db.json"
 
 
 def save_user(user_id, embedding):
+    """사용자 등록"""
     pass
 
 
 def load_db():
+    """데이터베이스 로드"""
     pass
 
 
-def get_user(user_id):
+def save_db(db):
+    """데이터베이스 저장"""
     pass
