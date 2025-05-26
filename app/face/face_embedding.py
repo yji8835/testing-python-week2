75f9a1efb7e27aaa1560680ab8635cbe1568ed3e
Jongin Yang <84954877+yji8835@users.noreply.github.com>
2025-05-26 15:12:52 +0900

    Update face_embedding.py

--- app/face/face_embedding.py
+++ app/face/face_embedding.py
@@ -1,8 +1,11 @@
 def extract_embedding(image):
     """이미지에서 임베딩 추출"""
-    pass
+    embedding = DeepFace.represent(img_path)[0]["embedding"]
+    return embedding
 
 
 def verify_embedding(embedding1, embedding2):
+def verify_embedding(embedding1: list, embedding2: list) -> bool:
     """두 임베딩이 같은 사람인지 검증"""
-    pass
+    is_same_person = DeepFace.verify(embedding1, embedding2)["verified"]
+    return is_same_person
